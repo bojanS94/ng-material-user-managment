65a2289e30a89013d0a1f2956fe826e701f25ccd
bojanS94 <eastcodewestcode94@gmail.com>
2022-08-28 14:20:02 +0200

    Project updated

--- src/app/components/user-add-dialog/user-add-dialog.component.ts
+++ src/app/components/user-add-dialog/user-add-dialog.component.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-add-dialog',
